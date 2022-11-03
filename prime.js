c52c9771853692ff17f7dcec0b272c577ebf666d
Sachin Sahu <sahusachin907@gmail.com>
2022-11-03 14:51:13 +0530

    code corrected by sachin

--- prime.js
+++ prime.js
@@ -1,8 +1,18 @@
-//function check if a No. is prime or not
 function checkprime(num){
-for(i=i;i<=num;i++);
-if(num%i){
-   return true;
-else
- return false;
-checkprime(num);
+let counter=0;
+for(i=1;i<=num;i++){
+if(num%i==0){
+	counter++;
+	}
+  }
+if(counter==2){
+return true;
+}
+return false;
+}
+let ans = checkprime(7);
+if(ans==true){
+console.log("Prime")
+}else{
+console.log("Not a Prime")
+}
